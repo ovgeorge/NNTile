a37ca6bcce1be5d7757ff1ff1f0e4d5420fdbf42
George Ovchinnikov <ovgeorge@yandex.ru>
2024-04-01 06:44:09 +0300

    Strassen: update test for python wrapper
    Previos test was for 2x2 matrices, which was not useful for finding some bugs.
    TODO:
     - Iterate over all possible transpose options

--- wrappers/python/tests/nntile_core/test_tensor_strassen.py
+++ wrappers/python/tests/nntile_core/test_tensor_strassen.py
@@ -14,6 +14,7 @@
 # All necesary imports
 import nntile
 import numpy as np
+
 # Set up StarPU configuration and init it
 config = nntile.starpu.Config(1, 0, 0)
 # Init all NNTile-StarPU codelets
@@ -21,66 +22,84 @@ nntile.starpu.init()
 # Define list of tested types
 dtypes = [np.float32, np.float64]
 # Define mapping between numpy and nntile types
-Tensor = {np.float32: nntile.tensor.Tensor_fp32,
-        np.float64: nntile.tensor.Tensor_fp64}
+Tensor = {np.float32: nntile.tensor.Tensor_fp32, np.float64: nntile.tensor.Tensor_fp64}
 # Define mapping between tested function and numpy type
-strassen = {np.float32: nntile.nntile_core.tensor.strassen_fp32,
-            np.float64: nntile.nntile_core.tensor.strassen_fp64}
+strassen = {
+    np.float32: nntile.nntile_core.tensor.strassen_fp32,
+    np.float64: nntile.nntile_core.tensor.strassen_fp64,
+}
+
 
 # Helper function returns bool value true if test passes
 def helper(dtype):
     # Describe single-tile tensor, located at node 0
-    matrix_shape = [2, 2]
+    matrix_shape = [6, 4]
     batch = 3
     mpi_distr = [0]
     next_tag = 0
-    shape = [*matrix_shape, batch]
+    alpha = 1
+    beta = 0
+
+    shape = [matrix_shape[0], 10, batch]
     traits = nntile.tensor.TensorTraits(shape, shape)
-    # Tensor objects
     A = Tensor[dtype](traits, mpi_distr, next_tag)
+    src_A = np.array(np.random.randn(*shape), dtype=dtype, order="F")
     next_tag = A.next_tag
+
+    shape = [10, matrix_shape[1], batch]
+    traits = nntile.tensor.TensorTraits(shape, shape)
     B = Tensor[dtype](traits, mpi_distr, next_tag)
+    src_B = np.array(np.random.randn(*shape), dtype=dtype, order="F")
     next_tag = B.next_tag
+
+    shape = [matrix_shape[0], matrix_shape[1], batch]
+    traits = nntile.tensor.TensorTraits(shape, shape)
     C = Tensor[dtype](traits, mpi_distr, next_tag)
+    src_C = np.array(np.random.randn(*shape), dtype=dtype, order="F")
+    dst_C = np.zeros_like(src_C, dtype=dtype, order="F")
+
     # Set initial values of tensors
-    src_A = np.array(np.random.randn(*shape), dtype=dtype, order='F')
-    src_B = np.array(np.random.randn(*shape), dtype=dtype, order='F')
-    src_C = np.array(np.random.randn(*shape), dtype=dtype, order='F')
-    dst_C = np.zeros_like(src_C)
     A.from_array(src_A)
     B.from_array(src_B)
     C.from_array(src_C)
-    # Get results by means of nntile and convert to numpy
-    alpha = 1
-    beta = -1
-    strassen[dtype](alpha, nntile.notrans, A, nntile.trans, B, beta, C, 1, 1, 0)
+
     C.to_array(dst_C)
+    strassen[dtype](alpha, nntile.notrans, A, nntile.notrans, B, beta, C, 1, 1, 0)
+    C.to_array(dst_C)
+
     nntile.starpu.wait_for_all()
     A.unregister()
     B.unregister()
     C.unregister()
+
     # Check results
     for i in range(batch):
         # Get result in numpy
-        src_C[:, :, i] = beta*src_C[:, :, i] + \
-                alpha*(src_A[:, :, i]@(src_B[:, :, i].T))
+        src_C[:, :, i] = beta * src_C[:, :, i] + alpha * (
+            src_A[:, :, i] @ (src_B[:, :, i])
+        )
         # Check if results are almost equal
-        if np.linalg.norm(dst_C[:, :, i]-src_C[:, :, i]) \
-                / np.linalg.norm(src_C[:, :, i]) > 1e-4:
+        if (
+            np.linalg.norm(dst_C[:, :, i] - src_C[:, :, i])
+            / np.linalg.norm(src_C[:, :, i])
+            > 1e-4
+        ):
             return False
     return True
+
 
 # Test runner for different precisions
 def test():
     for dtype in dtypes:
         assert helper(dtype)
 
+
 # Repeat tests
 def test_repeat():
     for dtype in dtypes:
         assert helper(dtype)
 
+
 if __name__ == "__main__":
     test()
     test_repeat()
-
